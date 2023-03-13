538fc6bb367da8719ebd36ec4f321750b019a735
Bruno Mattos <mattost14@gmail.com>
2023-03-13 14:55:01 -0600

    Add bACK

---
+++ computeThermal.m
@@ -0,0 +1,355 @@
+function [thermal,flag] = computeThermal(param)
+%COMPUTETHERMAL Summary of this function goes here
+%   Detailed explanation goes here
+
+    sigma = 5.67e-8; % Stefan-Boltzmann constant (W · m -2 · K -4 )
+    param.krad = param.emissivity*sigma;
+
+    % Mass paramters
+    param.facesMass = param.facesMassDistribution * (1-param.internalNode2TotalMassRatio) * param.satelliteMass;
+    param.internalNodeMass = param.internalNode2TotalMassRatio * param.satelliteMass;
+
+    % ### Internal loads ###
+    param.thermal.internalPwr = computeInternalPwr(param);
+
+    % ### Compute external thermal loads ###
+    % IR Radiation
+    switch param.IRmodel
+        case "Day/Night Temp"
+            param.thermal.faceIR = planetIRsimple(param);
+        case "Gradient"
+            param.thermal.faceIR = moonIRGradient(param);
+    end
+    % Solar Radiation (Direct)
+    param.thermal.faceSolar = computeSolarRadiation(param);
+    % Solar Radiation (Albedo)
+    param.thermal.faceAlbedo = computeAlbedoRadiation(param);
+
+
+    % ######## RUN SIMSCAPE MODEL ########
+    assignin('base', 'param', param)
+    warning('off')
+    out=sim("simpleThermalModel", param.orb.durationHours*3600);
+
+    % Return results
+    flag = 1;
+    thermal.faceIR = param.thermal.faceIR;
+    thermal.faceSolar = param.thermal.faceSolar;
+    thermal.faceAlbedo = param.thermal.faceAlbedo;
+    thermal.out = out;
+end
+
+function internalPwr = computeInternalPwr(param)
+    % This function computes the internal dissipation power profile based
+    % on inputs in the power tab
+
+    time = param.orb.prop.time_Epoch;
+    pwrDissipationDay = param.pwrDissipationDaySimpleModel;
+    pwrDissipationNight = param.pwrDissipationNightSimpleModel;
+    sunMagnitude = param.orb.prop.sunMagnitude;
+    switch param.pwrDissipationProfile
+        case "Constant"
+            internalPwr = pwrDissipationDay * ones(length(time),1);
+        case "Day/Night"
+            internalPwr = pwrDissipationDay .* sunMagnitude;
+            internalPwr = internalPwr +  pwrDissipationNight .* (1-sunMagnitude);
+    end
+
+    % Internal power as timetable
+    internalPwr = array2table(internalPwr);
+    internalPwr.time = seconds(time); 
+    internalPwr = table2timetable(internalPwr);
+end
+
+
+function faceIR = planetIRsimple(param)
+    % Constants
+    sigma = 5.67e-8; % Stefan-Boltzmann constant (W · m -2 · K -4 )
+
+    %%% Planetary IR Power
+    daytime = (param.orb.prop.albedoAngle.AlbedoAngle_deg_ - 90 < 0);
+    Pir = daytime*sigma*(param.Tir_Day)^4 + (1-daytime)*sigma*(param.Tir_Night)^4; % Central body radiation
+
+    % Compute IR flux at each face
+    for n=1:length(param.facesMaterial)
+        IR(:,n) = param.pwr.viewFactor(:,n) .* Pir * param.emissivity(n) * param.facesArea(n);
+    end
+
+    % Transform it to timetable for each face
+    time = param.orb.prop.time_Epoch;
+    % X+
+    faceIR.Xplus = array2table(IR(:,1));
+    faceIR.Xplus.time = seconds(time); 
+    faceIR.Xplus = table2timetable(faceIR.Xplus);
+    % X-
+    faceIR.Xminus = array2table(IR(:,2));
+    faceIR.Xminus.time = seconds(time); 
+    faceIR.Xminus = table2timetable(faceIR.Xminus);
+    % Y+
+    faceIR.Yplus = array2table(IR(:,3));
+    faceIR.Yplus.time = seconds(time); 
+    faceIR.Yplus = table2timetable(faceIR.Yplus);
+    % Y-
+    faceIR.Yminus = array2table(IR(:,4));
+    faceIR.Yminus.time = seconds(time); 
+    faceIR.Yminus = table2timetable(faceIR.Yminus);
+    % Z+
+    faceIR.Zplus = array2table(IR(:,5));
+    faceIR.Zplus.time = seconds(time); 
+    faceIR.Zplus = table2timetable(faceIR.Zplus);
+    % Z-
+    faceIR.Zminus = array2table(IR(:,6));
+    faceIR.Zminus.time = seconds(time); 
+    faceIR.Zminus = table2timetable(faceIR.Zminus);
+    
+end
+
+function faceIR = moonIRGradient(param)
+% This function computs each face IR input integratrin the Moon's surface
+% temperature distribution
+% The moon's surface temperature distribution is computed by the equation 
+% provided in the Human Landing System Lunar Thermal Analysis Guidebook, 
+% volume HLS-UG-001. NASA, 2021. (Pag. 17)
+
+    % Moon's surface emissivity
+    moonEmissivity = 0.96;
+    Rmoon = param.orb.prop.centralBodyRadius;
+    
+    % Satellite position, velocity at fixed frame
+    r_ff = param.orb.prop.r_ff;
+    v_ff = param.orb.prop.v_ff;
+    
+    % Sun position at fixed frame
+    sun_ff = param.orb.prop.Sun_ff;
+    
+    % function that return angle between two vectors in degrees
+    calculateAngleBetweenVectors = @(u,v) acosd(max(min(dot(u,v)/(norm(u)*norm(v)),1),-1));
+    
+    % Moon discretization steps
+    delta_lat = 1;                      % [degree]
+    delta_long = 1;                     % [degree]
+    LONG = -180:delta_long:180;
+    LAT = -90:delta_lat:90;
+
+    % Loop through time
+    time = param.orb.prop.time_Epoch;
+    IR = zeros(length(time), 6);
+    for i=1:length(time)
+         r_sat = r_ff(i,:)'; % sat position at time i
+         v_sat = v_ff(i,:)'; % sat velocity at time i
+         sun = sun_ff(i,:)'; % sun position at time i
+         % Loop through longitude angle (360 degrees)
+        for long=LONG
+            % Loop through latitude angle (180 degrees)
+            for lat = LAT  
+                
+                % Compute unit vector of the discrete area centered on the Moon
+                i_patch = [cosd(lat)*cosd(long); cosd(lat)*sind(long); sind(lat)];
+
+                % Surface patch area
+                dA = (Rmoon)^2 * cosd(lat) * deg2rad(delta_lat) * deg2rad(delta_long);    % units: m^2
+                
+                % Check if satellite is visible to patch
+                patch2sat = r_sat - i_patch * Rmoon;
+                satDeclinationAngle = calculateAngleBetweenVectors(i_patch, patch2sat);
+                if(satDeclinationAngle < 90 && dA~=0)
+    
+                    % Compute the subsolar latitude angle
+                    Lat_ss = 90-calculateAngleBetweenVectors(sun, i_patch);
+    
+                    % Compute QIR from patch 
+                    QIR = qIRsubsolar(Lat_ss, param.Albedo, param.orb.solarFlux, moonEmissivity, param.Tir_Night); % units: W/m2
+    
+                    % Compute view factors for each face
+                    F = computeViewFactorPatch2Face(r_sat, v_sat, i_patch, Rmoon, param);
+    
+                    % Compute accumulated IR flux at each face
+                    for n=1:6
+                        IR(i,n) = IR(i,n) + F(n) .* QIR * param.emissivity(n) * dA;
+                    end
+                end
+            end
+        end
+    end
+
+    % Transform it to timetable for each face
+    time = param.orb.prop.time_Epoch;
+    % X+
+    faceIR.Xplus = array2table(IR(:,1));
+    faceIR.Xplus.time = seconds(time); 
+    faceIR.Xplus = table2timetable(faceIR.Xplus);
+    % X-
+    faceIR.Xminus = array2table(IR(:,2));
+    faceIR.Xminus.time = seconds(time); 
+    faceIR.Xminus = table2timetable(faceIR.Xminus);
+    % Y+
+    faceIR.Yplus = array2table(IR(:,3));
+    faceIR.Yplus.time = seconds(time); 
+    faceIR.Yplus = table2timetable(faceIR.Yplus);
+    % Y-
+    faceIR.Yminus = array2table(IR(:,4));
+    faceIR.Yminus.time = seconds(time); 
+    faceIR.Yminus = table2timetable(faceIR.Yminus);
+    % Z+
+    faceIR.Zplus = array2table(IR(:,5));
+    faceIR.Zplus.time = seconds(time); 
+    faceIR.Zplus = table2timetable(faceIR.Zplus);
+    % Z-
+    faceIR.Zminus = array2table(IR(:,6));
+    faceIR.Zminus.time = seconds(time); 
+    faceIR.Zminus = table2timetable(faceIR.Zminus);
+
+end
+
+function F = computeViewFactorPatch2Face(r_sat, v_sat, i_patch, R, param)
+
+    % function that return angle between two vectors in degrees
+    calculateAngleBetweenVectors = @(u,v) acosd(max(min(dot(u,v)/(norm(u)*norm(v)),1),-1));
+
+    % Get face vector at LVLH frame
+    XplusFaceVector_LVLH = param.pwr.XplusFaceVector_LVLH;
+    YplusFaceVector_LVLH = param.pwr.YplusFaceVector_LVLH;
+    ZplusFaceVector_LVLH = param.pwr.ZplusFaceVector_LVLH;
+
+    % Get rotation matrix from LVLH to Fixed Frame
+    z_L2F = -r_sat/norm(r_sat);   % Unit vector z
+    h_F = cross(r_sat, v_sat);    % Angular momentum 
+    y_L2F = -h_F/norm(h_F);             % Unit vector y
+    x_L2F = cross(y_L2F, z_L2F);        % Unit vector x
+    A_F2L = [x_L2F'; y_L2F'; z_L2F'];   % Rotation matrix from Fixed Frame to LVLH frame
+    A_L2F = A_F2L';                     % Rotation matrix from LVLH frame to Fixed Frame
+    
+    % Get face vector at Fixed Frame
+    XplusFaceVector_F = A_L2F * XplusFaceVector_LVLH;
+    YplusFaceVector_F = A_L2F * YplusFaceVector_LVLH;
+    ZplusFaceVector_F = A_L2F * ZplusFaceVector_LVLH;
+
+    % View factor function (ref:
+    % http://imartinez.etsiae.upm.es/~isidoro/tc3/Radiation%20View%20factors.pdf,
+    % Eq. 1, Pag. 3)
+    computeViewFactor = @(beta1, beta2, A2, r12) cosd(beta1)*cosd(beta2)*A2/(pi*r12^2);
+
+    % ### Compute View Factor ###
+    patch2sat = r_sat - i_patch * R;
+    i_patch2sat = patch2sat/norm(patch2sat);
+    beta1 = calculateAngleBetweenVectors(i_patch, patch2sat);
+
+    beta2(1) =  calculateAngleBetweenVectors(XplusFaceVector_F, -i_patch2sat);
+    beta2(2) =  calculateAngleBetweenVectors(-XplusFaceVector_F, -i_patch2sat);
+    beta2(3) =  calculateAngleBetweenVectors(YplusFaceVector_F, -i_patch2sat);
+    beta2(4) =  calculateAngleBetweenVectors(-YplusFaceVector_F, -i_patch2sat);
+    beta2(5) =  calculateAngleBetweenVectors(ZplusFaceVector_F, -i_patch2sat);
+    beta2(6) =  calculateAngleBetweenVectors(-ZplusFaceVector_F, -i_patch2sat);
+
+    F = zeros(6,1);
+    for n=1:6
+        if(beta2(n)<90)
+            F(n) = computeViewFactor(beta1, beta2(n), param.facesArea(n), norm(patch2sat));
+        end
+    end
+end
+
+function qIR = qIRsubsolar(Latss, albedo, solarFlux, moonEmissivity, Tdark)
+% The moon's surface temperature distribution is computed by the equation 
+% provided in the Human Landing System Lunar Thermal Analysis Guidebook, 
+% volume HLS-UG-001. NASA, 2021. (Pag. 17)
+% Latss : degrees latitude from subsolar point (Subsolar Coordinate System)
+% albedo: Lunar surface albedo
+% solarFlux: solar flux (W/m2)
+% moonEmissivity: Lunar Surface Emissivity
+% Tdark: unilluminated lunar surface temperature
+
+    sigma = 5.670374419e-8; % W*m2*K^-4
+    if(Latss >= 0)
+        qIR = sind(Latss)*((1-albedo)*solarFlux - sigma * moonEmissivity * Tdark^4)+  sigma * moonEmissivity * Tdark^4;
+    else
+        qIR = sigma * moonEmissivity * Tdark^4;
+    end
+end
+
+
+function faceSolar = computeSolarRadiation(param)
+    solarFlux = param.orb.solarFlux;
+
+    % Compute solar flux at each face
+    for n=1:length(param.facesMaterial)
+        if(param.facesMaterial(n)=="Solar Panel") % If face is solar cell, check whether it is generating electrical energy or it is dissipating outside 
+            effectiveAbsoptivity =  param.absorptivity(n) - param.pwr.electricalEfficiency * param.solarCellEff(n);
+            solar(:,n) = param.pwr.solarLight.(n) .* solarFlux .* effectiveAbsoptivity * param.facesArea(n);
+        else
+            solar(:,n) = param.pwr.solarLight.(n) .* solarFlux * param.absorptivity(n) * param.facesArea(n);
+        end
+    end
+
+    % Transform it to timetable for each face
+    time = param.orb.prop.time_Epoch;
+    % X+
+    faceSolar.Xplus = array2table(solar(:,1));
+    faceSolar.Xplus.time = seconds(time); 
+    faceSolar.Xplus = table2timetable(faceSolar.Xplus);
+    % X-
+    faceSolar.Xminus = array2table(solar(:,2));
+    faceSolar.Xminus.time = seconds(time); 
+    faceSolar.Xminus = table2timetable(faceSolar.Xminus);
+    % Y+
+    faceSolar.Yplus = array2table(solar(:,3));
+    faceSolar.Yplus.time = seconds(time); 
+    faceSolar.Yplus = table2timetable(faceSolar.Yplus);
+    % Y-
+    faceSolar.Yminus = array2table(solar(:,4));
+    faceSolar.Yminus.time = seconds(time); 
+    faceSolar.Yminus = table2timetable(faceSolar.Yminus);
+    % Z+
+    faceSolar.Zplus = array2table(solar(:,5));
+    faceSolar.Zplus.time = seconds(time); 
+    faceSolar.Zplus = table2timetable(faceSolar.Zplus);
+    % Z-
+    faceSolar.Zminus = array2table(solar(:,6));
+    faceSolar.Zminus.time = seconds(time); 
+    faceSolar.Zminus = table2timetable(faceSolar.Zminus);
+end
+
+function faceAlbedo = computeAlbedoRadiation(param)
+    solarFlux = param.orb.solarFlux;
+    albedoFactor = param.Albedo;
+    
+    % Compute solar flux at each face
+    for n=1:length(param.facesMaterial)
+        % Albedo view factor (Falb) = Fir * cos(SZA)
+        Falb = param.pwr.viewFactor(:,n) .* max(0, cosd(param.orb.prop.albedoAngle.(1)));
+
+        if(param.facesMaterial(n)=="Solar Panel") % If face is solar cell, check whether it is generating electrical energy or it is dissipating outside 
+            effectiveAbsoptivity =  param.absorptivity(n) - param.pwr.electricalEfficiency * param.solarCellEff(n);
+            albedoRadiation(:,n) = albedoFactor * Falb .* solarFlux .* effectiveAbsoptivity * param.facesArea(n);
+        else
+            albedoRadiation(:,n) = albedoFactor * Falb .* solarFlux * param.absorptivity(n) * param.facesArea(n);
+        end
+    end
+
+    % Transform it to timetable for each face
+    time = param.orb.prop.time_Epoch;
+    % X+
+    faceAlbedo.Xplus = array2table(albedoRadiation(:,1));
+    faceAlbedo.Xplus.time = seconds(time); 
+    faceAlbedo.Xplus = table2timetable(faceAlbedo.Xplus);
+    % X-
+    faceAlbedo.Xminus = array2table(albedoRadiation(:,2));
+    faceAlbedo.Xminus.time = seconds(time); 
+    faceAlbedo.Xminus = table2timetable(faceAlbedo.Xminus);
+    % Y+
+    faceAlbedo.Yplus = array2table(albedoRadiation(:,3));
+    faceAlbedo.Yplus.time = seconds(time); 
+    faceAlbedo.Yplus = table2timetable(faceAlbedo.Yplus);
+    % Y-
+    faceAlbedo.Yminus = array2table(albedoRadiation(:,4));
+    faceAlbedo.Yminus.time = seconds(time); 
+    faceAlbedo.Yminus = table2timetable(faceAlbedo.Yminus);
+    % Z+
+    faceAlbedo.Zplus = array2table(albedoRadiation(:,5));
+    faceAlbedo.Zplus.time = seconds(time); 
+    faceAlbedo.Zplus = table2timetable(faceAlbedo.Zplus);
+    % Z-
+    faceAlbedo.Zminus = array2table(albedoRadiation(:,6));
+    faceAlbedo.Zminus.time = seconds(time); 
+    faceAlbedo.Zminus = table2timetable(faceAlbedo.Zminus);
+end
---
+++ initializeParameters.m
@@ -0,0 +1,232 @@
+%% ########## Orbit Tab ########
+app.param.orb.centralBody = "Earth";
+app.param.orb.solarFlux = 1361; % W/m2
+app.param.orb.sampleTime = 60; % (s)
+app.param.orb.sat = [];
+app.param.orb.startDate = datetime(date);
+app.param.orb.startHour = 0;
+app.param.orb.startMinute = 0;
+app.param.orb.durationHours = 1;
+app.param.orb.semiMajorAxis = 6778.137; % km
+app.param.orb.eccentricity = 0;
+app.param.orb.inclination = 52.580; % deg
+app.param.orb.RAAN = 190.158; % deg
+app.param.orb.argumentOfPeriapsis = 0; % deg
+app.param.orb.trueAnomaly = 32.690; % deg
+app.param.orb.prop.flag = 0;    
+app.param.Tir_Day = 254;% default: 254; % Black-body temp during day time (K)
+app.param.Tir_Night = 254;% default: 254; % Black-body temp during night time (K)
+app.param.Albedo = 0.3;
+app.param.earthIRmodelList = ["Day/Night Temp"];
+app.param.moonIRmodelList = ["Day/Night Temp"; "Gradient"];
+app.param.IRmodel = "Day/Night Temp";
+app.param.moonOrbPropagatorList = ["Kepler"; "Numerical (high precision)"];
+app.param.earthOrbPropagatorList = ["two-body-keplerian"; "sgp4"];
+app.param.orb.orbitPropagator = "two-body-keplerian";
+
+% Update fields value with inital parameters
+app.CentralBodyDropDown.Items = ["Earth"; "Moon"];
+app.IRmodelDropDown.Items = app.param.earthIRmodelList;
+app.CentralBodyDropDown.Value = app.param.orb.centralBody;
+app.IRmodelDropDown.Value = app.param.IRmodel;
+app.EpochDatePicker.Value = app.param.orb.startDate;
+app.SemimajoraxiskmEditField.Value = app.param.orb.semiMajorAxis;
+app.EccentricityEditField.Value = app.param.orb.eccentricity;
+app.InclinationEditField.Value = app.param.orb.inclination;
+app.RAANEditField.Value = app.param.orb.RAAN;
+app.ArgumentofPeriapsisEditField.Value = app.param.orb.argumentOfPeriapsis;
+app.TrueAnomalyEditField.Value = app.param.orb.trueAnomaly;
+app.SolarfluxWm2EditField.Value = app.param.orb.solarFlux;
+app.DayTempKEditField.Value = app.param.Tir_Day;
+app.NightTempKEditField.Value = app.param.Tir_Night;
+app.AlbedoEditField.Value = app.param.Albedo;
+app.PropagatorDropDown.Items = app.param.earthOrbPropagatorList;
+app.PropagatorDropDown.Value = app.param.orb.orbitPropagator;
+app.UpdateOrbitStatusLabel.Visible = "off";
+
+
+%% ########## Model Tab ########
+app.param.numberOfUnits = 1;
+
+itemList = getPropertiesFromMaterial("options");
+app.param.facesMaterial = [itemList(1), itemList(1), itemList(1), itemList(1), itemList(1), itemList(1)];
+app.param.satelliteMass = 1;
+app.param.solarCellEff = [.28, .28, .28, .28, .28, .28];
+app.param.effectiveAreaRatio = [.6, .6, .6, .6, .6, .6];
+app.param.resistance = [1, 1, 1, 1, 1, 1]; % K/W 
+app.param.internalNodeCp = 903; %J/kg/K
+app.param.internalNode2TotalMassRatio = .7;
+app.param.facesMassDistribution = [1/6, 1/6, 1/6, 1/6, 1/6, 1/6]; 
+app.param.facesArea = [1/6, 1/6, 1/6, 1/6, 1/6, 1/6]*(6*(0.1*0.1));
+app.param.attitude.nadirFace = "X+";
+app.param.attitude.ramFace = "Y+";
+app.param.deployable.type = ["None"; "None"; "None"; "None"; "None"; "None"];
+app.param.deployable.hinge = ["X+ | Y+"; "X- | Y+"; "Y+ | X+"; "Y- | X+"; "Z+ | X+"; "Z- | X+"];
+app.param.deployable.size = ["1P"; "1P"; "1P"; "1P"; "1P"; "1P"];
+app.param.deployable.solarCellEff = [.28, .28, .28, .28, .28, .28];
+app.param.deployable.effectiveAreaRatio = [.6, .6, .6, .6, .6, .6];
+app.param.deployable.flipFixedPanel = [0,0,0,0,0,0];
+
+
+% Face properties
+app.param.absorptivity = [getPropertiesFromMaterial(app.param.facesMaterial(1)).absorp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(2)).absorp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(3)).absorp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(4)).absorp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(5)).absorp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(6)).absorp]; 
+app.param.emissivity = [getPropertiesFromMaterial(app.param.facesMaterial(1)).emiss,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(2)).emiss,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(3)).emiss,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(4)).emiss,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(5)).emiss,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(6)).emiss];
+app.param.facesCp = [getPropertiesFromMaterial(app.param.facesMaterial(1)).Cp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(2)).Cp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(3)).Cp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(4)).Cp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(5)).Cp,...
+                        getPropertiesFromMaterial(app.param.facesMaterial(6)).Cp]; 
+
+% Update X+ face tab
+app.SurfaceTypeDropDownXplus.Items = itemList;
+p = getPropertiesFromMaterial(itemList(1));
+app.EmissivitySpinnerXplus.Value = p.emiss;
+app.EmissivitySpinnerXplus.Enable = "off";
+app.AbsorptivitySpinnerXplus.Value = p.absorp;
+app.AbsorptivitySpinnerXplus.Enable = "off";
+app.RatioEditFieldXplus.Value = p.absorp/p.emiss;
+app.CpSpinnerXplus.Value = p.Cp;
+app.CpSpinnerXplus.Enable = "off";
+app.HingeDropDownXplus.Enable = "off";
+app.HingeDropDownXplus.Items = ["X+ | Y+"; "X+ | Y-"; "X+ | Z+"; "X+ | Z-"];
+app.SizeDropDownXplus.Items = ["1P", "2P", "3P", "4P","5P","6P"];
+app.SizeDropDownXplus.Enable = "off";
+app.CellEfficiencySpinnerXplusDeployable.Enable = "off";
+app.EffecAreaRatioSpinnerXplusDeployable.Enable = "off";
+app.FlipPanelCheckBoxXplus.Enable = "off";
+
+
+% Update X- face tab
+app.SurfaceTypeDropDownXminus.Items = itemList;
+p = getPropertiesFromMaterial(itemList(1));
+app.EmissivitySpinnerXminus.Value = p.emiss;
+app.EmissivitySpinnerXminus.Enable = "off";
+app.AbsorptivitySpinnerXminus.Value = p.absorp;
+app.AbsorptivitySpinnerXminus.Enable = "off";
+app.RatioEditFieldXminus.Value = p.absorp/p.emiss;
+app.CpSpinnerXminus.Value = p.Cp;
+app.CpSpinnerXminus.Enable = "off";
+app.HingeDropDownXminus.Enable = "off";
+app.HingeDropDownXminus.Items = ["X- | Y+"; "X- | Y-"; "X- | Z+"; "X- | Z-"];
+app.SizeDropDownXminus.Items = ["1P", "2P", "3P", "4P","5P","6P"];
+app.SizeDropDownXminus.Enable = "off";
+app.CellEfficiencySpinnerXminusDeployable.Enable = "off";
+app.EffecAreaRatioSpinnerXminusDeployable.Enable = "off";
+app.FlipPanelCheckBoxXminus.Enable = "off";
+
+% Update Y+ face tab
+app.SurfaceTypeDropDownYplus.Items = itemList;
+p = getPropertiesFromMaterial(itemList(1));
+app.EmissivitySpinnerYplus.Value = p.emiss;
+app.EmissivitySpinnerYplus.Enable = "off";
+app.AbsorptivitySpinnerYplus.Value = p.absorp;
+app.AbsorptivitySpinnerYplus.Enable = "off";
+app.RatioEditFieldYplus.Value = p.absorp/p.emiss;
+app.CpSpinnerYplus.Value = p.Cp;
+app.CpSpinnerYplus.Enable = "off";
+app.HingeDropDownYplus.Enable = "off";
+app.HingeDropDownYplus.Items = ["Y+ | X+"; "Y+ | X-"; "Y+ | Z+"; "Y+ | Z-"];
+app.SizeDropDownYplus.Items = ["1P", "2P", "3P", "4P","5P","6P"];
+app.SizeDropDownYplus.Enable = "off";
+app.CellEfficiencySpinnerYplusDeployable.Enable = "off";
+app.EffecAreaRatioSpinnerYplusDeployable.Enable = "off";
+app.FlipPanelCheckBoxYplus.Enable = "off";
+
+% Update Y- face tab
+app.SurfaceTypeDropDownYminus.Items = itemList;
+p = getPropertiesFromMaterial(itemList(1));
+app.EmissivitySpinnerYminus.Value = p.emiss;
+app.EmissivitySpinnerYminus.Enable = "off";
+app.AbsorptivitySpinnerYminus.Value = p.absorp;
+app.AbsorptivitySpinnerYminus.Enable = "off";
+app.RatioEditFieldYminus.Value = p.absorp/p.emiss;
+app.CpSpinnerYminus.Value = p.Cp;
+app.CpSpinnerYminus.Enable = "off";
+app.HingeDropDownYminus.Enable = "off";
+app.HingeDropDownYminus.Items = ["Y- | X+"; "Y- | X-"; "Y- | Z+"; "Y- | Z-"];
+app.SizeDropDownYminus.Items = ["1P", "2P", "3P", "4P","5P","6P"];
+app.SizeDropDownYminus.Enable = "off";
+app.CellEfficiencySpinnerYminusDeployable.Enable = "off";
+app.EffecAreaRatioSpinnerYminusDeployable.Enable = "off";
+app.FlipPanelCheckBoxYminus.Enable = "off";
+
+% Update Z+ face tab
+app.SurfaceTypeDropDownZplus.Items = itemList;
+p = getPropertiesFromMaterial(itemList(1));
+app.EmissivitySpinnerZplus.Value = p.emiss;
+app.EmissivitySpinnerZplus.Enable = "off";
+app.AbsorptivitySpinnerZplus.Value = p.absorp;
+app.AbsorptivitySpinnerZplus.Enable = "off";
+app.RatioEditFieldZplus.Value = p.absorp/p.emiss;
+app.CpSpinnerZplus.Value = p.Cp;
+app.CpSpinnerZplus.Enable = "off";
+app.HingeDropDownZplus.Enable = "off";
+app.HingeDropDownZplus.Items = ["Z+ | X+"; "Z+ | X-"; "Z+ | Y+"; "Z+ | Y-"];
+app.SizeDropDownZplus.Items = ["1P", "2P", "3P", "4P","5P","6P"];
+app.SizeDropDownZplus.Enable = "off";
+app.CellEfficiencySpinnerZplusDeployable.Enable = "off";
+app.EffecAreaRatioSpinnerZplusDeployable.Enable = "off";
+app.FlipPanelCheckBoxZplus.Enable = "off";
+
+% Update Z- face tab
+app.SurfaceTypeDropDownZminus.Items = itemList;
+p = getPropertiesFromMaterial(itemList(1));
+app.EmissivitySpinnerZminus.Value = p.emiss;
+app.EmissivitySpinnerZminus.Enable = "off";
+app.AbsorptivitySpinnerZminus.Value = p.absorp;
+app.AbsorptivitySpinnerZminus.Enable = "off";
+app.RatioEditFieldZminus.Value = p.absorp/p.emiss;
+app.CpSpinnerZminus.Value = p.Cp;
+app.CpSpinnerZminus.Enable = "off";
+app.HingeDropDownZminus.Enable = "off";
+app.HingeDropDownZminus.Items = ["Z- | X+"; "Z- | X-"; "Z- | Y+"; "Z- | Y-"];
+app.SizeDropDownZminus.Items = ["1P", "2P", "3P", "4P","5P","6P"];
+app.SizeDropDownZminus.Enable = "off";
+app.CellEfficiencySpinnerZminusDeployable.Enable = "off";
+app.EffecAreaRatioSpinnerZminusDeployable.Enable = "off";
+app.FlipPanelCheckBoxZminus.Enable = "off";
+
+% Attitude
+app.NadirFaceDropDown.Items = ["X+"; "X-"; "Y+"; "Y-"; "Z+"; "Z-"];
+app.NadirFaceDropDown.Value = app.param.attitude.nadirFace;
+app.RamFaceDropDown.Items = ["Y+"; "Y-"; "Z+"; "Z-"];
+app.RamFaceDropDown.Value = app.param.attitude.ramFace;
+
+
+%% ########## Power Tab ########
+
+app.param.pwrDissipationProfileList = ["Constant"; "Day/Night"];
+app.param.pwrDissipationProfile = app.param.pwrDissipationProfileList(1);
+app.param.pwrDissipationDaySimpleModel = 1;      % W
+app.param.pwrDissipationNightSimpleModel = 1;    % W
+app.param.pwr.flag = 0; 
+
+% Update fields value with inital parameters
+app.ProfileDropDown.Items = app.param.pwrDissipationProfileList;
+app.ProfileDropDown.Value = app.param.pwrDissipationProfile;
+app.PwrDayWSpinner.Value = app.param.pwrDissipationDaySimpleModel;
+app.PwrNightWSpinner.Value = app.param.pwrDissipationNightSimpleModel;
+app.PwrNightWSpinner.Enable = "off";
+app.ComputePowerStatusLabel.Visible = "off";
+
+%% ########## Thermal Tab ########
+app.param.thermal.flag = 0; 
+app.param.plot.solarRadiationInput.showFacesFlag = 0;
+app.param.plot.IRRadiationInput.showFacesFlag = 0;
+app.param.plot.albedoRadiationInput.showFacesFlag = 0;
+
+% Update fields value with inital parameters
+app.ComputeThermalStatusLabel.Visible = "off";
+
+
