f5432f42e43f541c738811d13d0847fdba1b5709
Bruno Mattos <mattost14@gmail.com>
2023-03-13 16:06:38 -0600

    Change on plotCubesat

--- plotCubesat.m
+++ plotCubesat.m
@@ -17,25 +17,7 @@ function plotCubesat(param)
     h = surf([-3, 3; -3, 3],[-3, -3; 3, 3], [-3, -3; -3, -3], 'CData',getTexture(param.centralBody), 'FaceColor','texturemap', 'edgecolor','#656565', 'LineWidth', 3);
 
     axis([ -1  1  -1  1 -1  1]*3)
-%     xlabel('Y - ZcrossX')
-%     ylabel('Z - Orbital Normal')
-%     zlabel('X - Orbital Radial')
     grid on
     axis vis3d
     
-%     Xbody = angle2dcm(deg2rad(param.attitude.rotationAboutZ), deg2rad(param.attitude.rotationAboutY), deg2rad(param.attitude.rotationAboutX),'ZYX')'*X'*scale;
-%     Ybody = angle2dcm(deg2rad(param.attitude.rotationAboutZ), deg2rad(param.attitude.rotationAboutY), deg2rad(param.attitude.rotationAboutX),'ZYX')'*Y'*scale;
-%     Zbody = angle2dcm(deg2rad(param.attitude.rotationAboutZ), deg2rad(param.attitude.rotationAboutY), deg2rad(param.attitude.rotationAboutX),'ZYX')'*Z'*scale;
-%       
-%     q=quiver3([0],[0],[0],Xbody(1),Xbody(2),Xbody(3), 'Color','g','LineWidth',5);
-%     text(Xbody(1),Xbody(2),Xbody(3),'X+','HorizontalAlignment','left','FontSize',28);
-%     
-%     q=quiver3([0],[0],[0],Ybody(1),Ybody(2),Ybody(3), 'Color','r','LineWidth',5);
-%     t=text(Ybody(1),Ybody(2),Ybody(3),'Y+','HorizontalAlignment','left','FontSize',28);
-% 
-%     q=quiver3([0],[0],[0],Zbody(1),Zbody(2),Zbody(3), 'Color','b','LineWidth',5);
-%     text(Zbody(1),Zbody(2),Zbody(3),'Z+','HorizontalAlignment','left','FontSize',28);
-%     
-%     set(gca, 'Xdir', 'reverse')
-%     set(gca, 'Ydir', 'reverse')
 end
