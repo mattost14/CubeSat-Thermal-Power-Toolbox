b68ed358d31078581e7384e3baa6f0dc10d76934
Bruno Mattos <mattost14@gmail.com>
2023-09-22 11:48:07 -0600

    Added the 8U CubeSat option

--- PlotFunctions/getCubeSatPlotPoints.m
+++ PlotFunctions/getCubeSatPlotPoints.m
@@ -13,6 +13,10 @@ function P = getCubeSatPlotPoints(numberOfUnits)
             numberUnitAlongX = 3;
             numberUnitAlongY = 2;
             numberUnitAlongZ = 1;
+        case 8
+            numberUnitAlongX = 4;
+            numberUnitAlongY = 2;
+            numberUnitAlongZ = 1;
         case 12
             numberUnitAlongX = 3;
             numberUnitAlongY = 2;
@@ -107,6 +111,35 @@ function P = getCubeSatPlotPoints(numberOfUnits)
             x_faceZ = [ -1.5, -.5, .5, 1.5; -1.5, -.5, .5, 1.5; -1.5, -.5, .5, 1.5;];
             y_faceZ = [ -1, -1, -1, -1; 0, 0, 0, 0; 1, 1, 1, 1];
             z_faceZ = [ones(1,length(y_faceZ)); ones(1,length(y_faceZ)); ones(1,length(y_faceZ)) ]*(.5);
+            faceZ_plus.x = x_faceZ;
+            faceZ_plus.y = y_faceZ;
+            faceZ_plus.z = z_faceZ;
+            faceZ_minus = faceZ_plus;
+            faceZ_minus.z = -faceZ_minus.z;
+            P = [faceX_plus, faceX_minus, faceY_plus, faceY_minus, faceZ_plus, faceZ_minus];
+        case 8
+            % X face
+            x_faceX = [ 2, 2, 2; 2, 2, 2];
+            y_faceX = [ -1, 0, 1; -1, 0, 1];
+            z_faceX = [0.5, 0.5, 0.5; -0.5, -0.5, -0.5];
+            faceX_plus.x = x_faceX;
+            faceX_plus.y = y_faceX;
+            faceX_plus.z = z_faceX;
+            faceX_minus = faceX_plus;
+            faceX_minus.x = -faceX_minus.x;
+            % Y face
+            x_faceY = [ -2, -1, 0, 1, 2; -2, -1, 0, 1, 2];
+            y_faceY = [ 1, 1, 1, 1, 1; 1 ,1, 1, 1, 1];
+            z_faceY = [ 0.5, 0.5, 0.5, 0.5, 0.5; -0.5, -0.5, -0.5, -0.5, -0.5];
+            faceY_plus.x = x_faceY;
+            faceY_plus.y = y_faceY;
+            faceY_plus.z = z_faceY;
+            faceY_minus = faceY_plus;
+            faceY_minus.y = -faceY_minus.y;
+            % Z face
+            x_faceZ = [ -2, -1, 0, 1, 2; -2, -1, 0, 1, 2; -2, -1, 0, 1, 2];
+            y_faceZ = [ 1, 1, 1, 1, 1; 0, 0, 0, 0, 0; -1, -1, -1, -1, -1];
+            z_faceZ = [ 0.5, 0.5, 0.5, 0.5, 0.5; 0.5, 0.5, 0.5, 0.5, 0.5; 0.5, 0.5, 0.5, 0.5, 0.5];
             faceZ_plus.x = x_faceZ;
             faceZ_plus.y = y_faceZ;
             faceZ_plus.z = z_faceZ;
